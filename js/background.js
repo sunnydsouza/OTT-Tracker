806452b5560f9920e1a2b3a03c16c0a8bd1556de
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-04-28 19:19:38 +0530

    refactor(extension): refactored background.js. Added icons to extension

--- js/background.js
+++ js/background.js
@@ -1,96 +1,99 @@
-fetch("../README.md")
-  .then((response) => response.text())
-  .then((data) => {
-    // Do something with your data
-    console.log(data);
-  });
-
-const dateMap = new Map();
-const hourMap = new Map();
-const hourlyWindowMap = new Map();
-const windowStatsMap = new Map();
+const dateMap = {};
+const hourMap = {};
+const hourlyWindowMap = {};
+const windowStatsMap = {};
 
 function findAudioTabs() {
   chrome.tabs.query({ audible: !0 }, function (tabs) {
     tabs.forEach((element) => {
       let eventDateTime = new Date().toLocaleString();
-      const eventDate = eventDateTime.substr(0, 10);
-      const hour = eventDateTime.substr(12, 2);
+      const eventDate = eventDateTime.substring(0, 10);
+      const hour = eventDateTime.substring(12, 14);
       const windowTitle = element.title;
       console.log(
         "Processing for tab ->" +
           windowTitle +
-          "eventDate:" +
+          " -> eventDate:" +
           eventDate +
-          " hour:" +
+          ", hour:" +
           hour
       );
+
+      if (!(windowTitle in getHourlyWindow(hour))) {
+        windowStatsMap[windowTitle] = []; //reset for the hour
+      }
       getWindowExistingStats(windowTitle).push(eventDateTime);
-
       getHourlyWindow(hour)[windowTitle] = getWindowExistingStats(windowTitle);
-      getHour(hour)[hour] = getHourlyWindow(hour);
-      getDate(eventDate)[eventDate] = getHour(hour);
+      //   getHour(hour)[hour] = getHourlyWindow(hour);
+      getDate(eventDate)[eventDate] = hourlyWindowMap;
 
-      //   console.log(
-      //     "current value of windowStatsMap is:" +
-      //       JSON.stringify(getWindowExistingStats(windowTitle))
-      //   );
-      //   console.log(
-      //     "current value of hourMap is:" + JSON.stringify(getHour(hour))
-      //   );
       console.log(
         "current value of dateMap is:" + JSON.stringify(getDate(eventDate))
       );
-
-      //   console.log(
-      //     eventDateTime + " => " + element.url + " => " + element.title
-      //   );
-      //   chrome.storage.sync.set({ hourlyStats: eventDateTime }, function () {
-      //     console.log("Value is set to " + eventDateTime);
-      //   });
-
-      //   chrome.storage.sync.get(['key'], function(result) {
-      //     console.log('Value currently is ' + result.key);
-      //   });
     });
-    // var url = tabs[0].url;
-    // console.log(url);
   });
 }
 
-// const getDate = (date) => dateMap.get(date) || dateMap.get("new");
-// const getHour = (hour) => hourMap.get(hour) || hourMap.get("new");
-// const getHourlyWindow = (hour) =>
-//   hourlyWindowMap.get(hour) || hourlyWindowMap.get("new");
-const getDate = (date) => {
-  if (!dateMap.has(date)) dateMap.set(date, {});
+//Get settings from storage
+function getSettings() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(
+      {
+        syncTime: 900,
+        monitorTime: 90,
+        useApiEndpoint: false,
+        apiEndpoint: "",
+      },
+      function (items) {
+        settings = JSON.parse(JSON.stringify(items));
+        return resolve(settings);
+      }
+    );
+  });
+}
 
-  return dateMap.get(date);
+var settings = getSettings();
+
+const getDate = (date) => {
+  if (!(date in dateMap)) dateMap[date] = {};
+
+  return dateMap[date];
 };
+
 const getHour = (hour) => {
-  if (!hourMap.has(hour)) hourMap.set(hour, {});
-  return hourMap.get(hour);
+  if (!(hour in hourMap)) hourMap[hour] = {};
+  return hourMap[hour];
 };
 const getHourlyWindow = (hour) => {
-  if (!hourlyWindowMap.has(hour)) hourlyWindowMap.set(hour, {});
-  return hourlyWindowMap.get(hour);
+  if (!(hour in hourlyWindowMap)) hourlyWindowMap[hour] = {};
+  return hourlyWindowMap[hour];
 };
 const getWindowExistingStats = (window) => {
-  if (!windowStatsMap.has(window)) windowStatsMap.set(window, []);
-  return windowStatsMap.get(window);
+  if (!(window in windowStatsMap)) {
+    windowStatsMap[window] = [];
+  }
+  return windowStatsMap[window];
 };
 
 function getCurrentStorageValue() {
-  chrome.storage.sync.get(null, function (items) {
+  chrome.storage.sync.get(["hourlyStats"], function (items) {
     var result = JSON.stringify(items);
     console.log("Storage results:" + result);
   });
 }
 
+function setChromeStorgeValue() {
+  chrome.storage.sync.set({ hourlyStats: dateMap }, function () {
+    console.log("Set Storage Value: 'hourlyStats' is set to \n" + dateMap);
+  });
+}
+
 setInterval(function () {
+  console.log(settings);
   findAudioTabs();
+  setChromeStorgeValue();
   getCurrentStorageValue();
-}, 60000);
+}, 5000);
 
 // setInterval(function () {
 //     saveToDisk();
@@ -129,63 +132,22 @@ async function postData(url = "", data = {}) {
 }
 
 setInterval(function () {
-  console.log(JSON.stringify(Object.fromEntries(dateMap)));
-  console.log(
-    "Outer value of dateMap for 22/04/2022:" +
-      JSON.stringify(getDate("22/04/2022"))
-  );
-  postData("http://192.168.0.109:1880/answer", { answer: Object.fromEntries(dateMap) }).then(
-    (data) => {
-      console.log(data); // JSON data parsed by `data.json()` call
-    }
-  );
+  console.log(JSON.stringify(dateMap));
+
+  postData("http://192.168.0.109:1880/answer", {
+    answer: dateMap,
+  }).then((data) => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
 }, 900000);
-// chrome.storage.local.get(null, function(items) { // null implies all items
-//     // Convert object to a string.
-//     var result = JSON.stringify(items);
 
-//     // Save as file
-//     var url = 'data:application/json;base64,' + btoa(result);
-//     chrome.downloads.download({
-//         url: url,
-//         filename: 'filename_of_exported_file.json'
-//     });
-// });
-
-//  logFile = path.join(userhome('.active-win-log'), 'test234.json')
-// async function getLogFileData() {
-//     const logFile = await readFileAsync(logFile);
-//     return JSON.parse(logFile);
-//   };
-
-//   async function setLogFileData(jsObj) {
-//     await writeFileAsync(logFile, JSON.stringify(jsObj, null, '\t'));
-//   }
-// $(document).ready(findAudioTabs);
-// chrome.tabs.onUpdated.addListener(findAudioTabs);
-// chrome.tabs.onCreated.addListener(findAudioTabs);
-// chrome.tabs.query(
-//     {
-//       audible: !0,
-//     },
-//     a => {
-//         a.sort((c, a) => a.id - c.id), document.querySelector(".tabs__title").textContent = 0 < a.length ? "Tabs playing audio right now" : "No tabs playing audio right now", a.forEach(a => {
-//             const b = document.getElementById("template-tab")
-//                 .content;
-//             b.querySelector(".tab")
-//                 .dataset.tabId = a.id, b.querySelector(".tab__icon-image")
-//                 .src = a.favIconUrl, b.querySelector(".tab__title")
-//                 .textContent = a.title, document.querySelector(".tabs__list").appendChild(document.importNode(b, !0))
-//         })
-//     });
-
-
+//  #######  Below code is to restrict from the chrome background service worker from going inactive #######
 let lifeline;
 
 keepAlive();
 
-chrome.runtime.onConnect.addListener(port => {
-  if (port.name === 'keepAlive') {
+chrome.runtime.onConnect.addListener((port) => {
+  if (port.name === "keepAlive") {
     lifeline = port;
     setTimeout(keepAliveForced, 295e3); // 5 minutes minus 5 seconds
     port.onDisconnect.addListener(keepAliveForced);
@@ -200,11 +162,11 @@ function keepAliveForced() {
 
 async function keepAlive() {
   if (lifeline) return;
-  for (const tab of await chrome.tabs.query({ url: '*://*/*' })) {
+  for (const tab of await chrome.tabs.query({ url: "*://*/*" })) {
     try {
       await chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: () => chrome.runtime.connect({ name: 'keepAlive' }),
+        function: () => chrome.runtime.connect({ name: "keepAlive" }),
         // `function` will become `func` in Chrome 93+
       });
       chrome.tabs.onUpdated.removeListener(retryOnTabUpdate);
