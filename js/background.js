d75fb1b0c64aef0e8a1e35fdc21f72a74c62e296
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-04-29 07:03:41 +0530

    feat(interval): introduced interval based logging.
    More meaningful and compact output. Now you can calculate durations directly from the intervals.

--- js/background.js
+++ js/background.js
@@ -1,14 +1,15 @@
 const dateMap = {};
-const hourMap = {};
-const hourlyWindowMap = {};
 const windowStatsMap = {};
 
+/**
+ * Find the tabs playing audio and create a map of the tabs and their details
+ */
 function findAudioTabs() {
+  let eventDateTime = new Date().toLocaleString();
+  const eventDate = eventDateTime.substring(0, 10);
+  const hour = eventDateTime.substring(12, 14);
   chrome.tabs.query({ audible: !0 }, function (tabs) {
     tabs.forEach((element) => {
-      let eventDateTime = new Date().toLocaleString();
-      const eventDate = eventDateTime.substring(0, 10);
-      const hour = eventDateTime.substring(12, 14);
       const windowTitle = element.title;
       console.log(
         "Processing for tab ->" +
@@ -19,19 +20,141 @@ function findAudioTabs() {
           hour
       );
 
-      if (!(windowTitle in getHourlyWindow(hour))) {
-        windowStatsMap[windowTitle] = []; //reset for the hour
+      if (getWindowExistingStats(windowTitle).length == 0) {
+        //If there is no window tracking for this window title
+        getWindowExistingStats(windowTitle).push(getNewInterval(eventDateTime));
+      } else if (
+        getWindowExistingStats(windowTitle)[
+          getWindowExistingStats(windowTitle).length - 1
+        ].end_time != ""
+      ) {
+        //If there is an existing window tracking for this window title but if the last interval for this window title is closed, create a new interval
+        getWindowExistingStats(windowTitle).push(getNewInterval(eventDateTime));
       }
-      getWindowExistingStats(windowTitle).push(eventDateTime);
-      getHourlyWindow(hour)[windowTitle] = getWindowExistingStats(windowTitle);
-      //   getHour(hour)[hour] = getHourlyWindow(hour);
-      getDate(eventDate)[eventDate] = hourlyWindowMap;
+
+      getDate(eventDate)[eventDate] = windowStatsMap;
 
       console.log(
-        "current value of dateMap is:" + JSON.stringify(getDate(eventDate))
+        "current value of dateMap is:" + JSON.stringify(getDate2(eventDate))
       );
     });
+
+    weedNoLongerPlayingTabs(eventDate, tabs);
   });
+}
+
+const getDate = (date) => {
+  if (!(date in dateMap)) dateMap[date] = {};
+
+  return dateMap[date];
+};
+const getWindowExistingStats = (window) => {
+  if (!(window in windowStatsMap)) {
+    windowStatsMap[window] = [];
+  }
+  return windowStatsMap[window];
+};
+
+/**
+ * A new interval is created for each tab that is opened.
+ * @param {String} eventDateTime The date the event occured. Ideally the current date/time
+ * @returns
+ */
+function getNewInterval(eventDateTime) {
+  const intervalMap = {};
+  intervalMap["start_time"] = eventDateTime;
+  intervalMap["end_time"] = "";
+  return intervalMap;
+}
+
+/**
+ * Get all the window titles for the current date
+ * @param {String} eventDate The date the event occured. Ideally the current date/time
+ * @returns
+ */
+function getAllWindowTitlesForDate(eventDate) {
+  const windowTitles = Object.values(getDate(eventDate)).map((tabs) =>
+    Object.keys(tabs)
+  )[0]; //TODO need to check of a better way to achieve this
+  return windowTitles;
+}
+
+/**
+ * Get all the window details for the current date.
+ * Ideally the same as {getAllWindowTitlesForDate} but this contains the window details
+ * @param {String} eventDate The date the event occured. Ideally the current date/time
+ * @returns
+ */
+function getWindowMapForDate(eventDate) {
+  const windowMap = Object.values(getDate(eventDate)); //TODO need to check of a better way to achieve this
+  return windowMap;
+}
+
+/**
+ * Compares all the window titles for the current date and closes the window stats for any that are no longer active.
+ * @param {String} eventDate The date the event occured. Ideally the current date/time
+ * @param {tabs} currentActiveTabs The tabs that are currently active, passed from the callback from chrome.tabs.query
+ */
+function weedNoLongerPlayingTabs(eventDate, currentActiveTabs) {
+  // Get all the window titles for the current date
+  const currentActiveTabsTitles = currentActiveTabs.map((tab) => tab.title);
+
+  // Check the difference between the current active tabs and the window titles for the current date
+  console.log(getAllWindowTitlesForDate(eventDate));
+  if (getAllWindowTitlesForDate(eventDate)!=undefined) {
+    let noLongerPlayingTabs = getAllWindowTitlesForDate(eventDate).filter(
+      (x) =>
+        !currentActiveTabsTitles.includes(x) &&
+        hasAnActiveWindowTracking(eventDate, x)
+    );
+    // let difference = currentActiveTabsTitles.filter(x => !getAllWindowTitlesForDate(eventDate).includes(x));
+
+    console.log("Difference offset");
+    console.log(noLongerPlayingTabs);
+
+    //Close the window stats for any that are no longer active
+    closeWindowIntervalForExistingTabs(noLongerPlayingTabs);
+  } else {
+    console.log("No window titles for this date. Nothing to weed out");
+  }
+}
+
+/**
+ * Close the window stats for any that are no longer active
+ * i.e: if the intervalMap for the window has a start_time that is not empty but an end_time is empty
+ * this will put the latest timestamp for the end_time for the window.(the interval is now closed!)
+ * @param {Array<String>} noLongerPlayingTabs
+ */
+function closeWindowIntervalForExistingTabs(noLongerPlayingTabs) {
+  noLongerPlayingTabs.forEach((tabTitle) => {
+    console.log("Closing window stats for tab:" + tabTitle);
+    getWindowExistingStats(tabTitle)[
+      getWindowExistingStats(tabTitle).length - 1
+    ].end_time = new Date().toLocaleString();
+  });
+}
+
+/**
+ * Checks if the window title has an active window tracking. i.e: if the intervalMap for the window has
+ * a start_time that is not empty but an end_time is empty.(still running!)
+ * @param {String} eventDate
+ * @param {String} windowTitle
+ * @returns
+ */
+function hasAnActiveWindowTracking(eventDate, windowTitle) {
+  return (
+    getWindowMapForDate(eventDate).filter((windowMap) => {
+      if (
+        windowMap[windowTitle][windowMap[windowTitle].length - 1].end_time == ""
+      ) {
+        console.log(
+          "there is an active window session for this window title:" +
+            windowTitle
+        );
+        return true;
+      }
+    }).length > 0
+  );
 }
 
 //Get settings from storage
@@ -54,27 +177,6 @@ function getSettings() {
 
 var settings = getSettings();
 
-const getDate = (date) => {
-  if (!(date in dateMap)) dateMap[date] = {};
-
-  return dateMap[date];
-};
-
-const getHour = (hour) => {
-  if (!(hour in hourMap)) hourMap[hour] = {};
-  return hourMap[hour];
-};
-const getHourlyWindow = (hour) => {
-  if (!(hour in hourlyWindowMap)) hourlyWindowMap[hour] = {};
-  return hourlyWindowMap[hour];
-};
-const getWindowExistingStats = (window) => {
-  if (!(window in windowStatsMap)) {
-    windowStatsMap[window] = [];
-  }
-  return windowStatsMap[window];
-};
-
 function getCurrentStorageValue() {
   chrome.storage.sync.get(["hourlyStats"], function (items) {
     var result = JSON.stringify(items);
@@ -89,6 +191,7 @@ function setChromeStorgeValue() {
 }
 
 setInterval(function () {
+  settings = getSettings(); //refresh the settings, on the go
   console.log(settings);
   findAudioTabs();
   setChromeStorgeValue();
--- js/options.js
+++ js/options.js
@@ -1,83 +1,89 @@
 function saveOptions() {
-    var syncTime = document.getElementById('sync_time').value;
-    var monitorTime = document.getElementById('monitor_time').value;
-    var useApiEndpoint = document.getElementById('use_api_endpoint').checked;
-    var apiEndpoint = document.getElementById('api_endpoint').value;
-    var manualDownloadData = document.getElementById('manual_download_data').value;
+  var syncTime = document.getElementById("sync_time").value;
+  var monitorTime = document.getElementById("monitor_time").value;
+  var useApiEndpoint = document.getElementById("use_api_endpoint").checked;
+  var apiEndpoint = document.getElementById("api_endpoint").value;
+  var manualDownloadData = document.getElementById(
+    "manual_download_data"
+  ).value;
 
-    chrome.storage.sync.set({
-        syncTime: syncTime,
-        monitorTime: monitorTime,
-        useApiEndpoint: useApiEndpoint,
-        apiEndpoint: apiEndpoint
-    });
+  chrome.storage.sync.set({
+    syncTime: syncTime,
+    monitorTime: monitorTime,
+    useApiEndpoint: useApiEndpoint,
+    apiEndpoint: apiEndpoint,
+  });
 }
 
 function restore_options() {
-   
-    chrome.storage.sync.get({
-        syncTime: 900,
-        monitorTime: 90,
-        useApiEndpoint: false,
-        apiEndpoint: ""
+  chrome.storage.sync.get(
+    {
+      syncTime: 900,
+      monitorTime: 90,
+      useApiEndpoint: false,
+      apiEndpoint: "",
+    },
+    function (items) {
+      document.getElementById("sync_time").value = items.syncTime;
+      document.getElementById("monitor_time").value = items.monitorTime;
+      document.getElementById("use_api_endpoint").checked =
+        items.useApiEndpoint;
+      document.getElementById("api_endpoint").value = items.apiEndpoint;
 
-    }, function (items) {
-        document.getElementById('sync_time').value = items.syncTime;
-        document.getElementById('monitor_time').value = items.monitorTime;
-        document.getElementById('use_api_endpoint').checked = items.useApiEndpoint;
-        document.getElementById('api_endpoint').value = items.apiEndpoint;
-        
+      //Api endpoint sync
+      // if (document.getElementById('use_api_endpoint').checked == false ) {
+      //     document.getElementById('change_slack_status').disabled = true;
+      // }
+    }
+  );
+  //Set listeners
 
-        //Api endpoint sync
-        // if (document.getElementById('use_api_endpoint').checked == false ) {
-        //     document.getElementById('change_slack_status').disabled = true;   
-        // }
-    });
-    //Set listeners
+  document
+    .getElementById("use_api_endpoint")
+    .addEventListener("change", function () {
+      if (this.checked) {
+        //validate the api endpoint cannot be empty
+        document
+          .getElementById("api_endpoint")
+          .setAttribute("required", "required");
 
-    document.getElementById('use_api_endpoint').addEventListener('change', function () {
-        if (this.checked) {
-            //validate the api endpoint cannot be empty
-            document.getElementById('api_endpoint').setAttribute('required', 'required');
-          
-
-            document.getElementById('options').classList.add("was-validated");
-        } else {
-            document.getElementById('api_endpoint').value = "";
-            document.getElementById('api_endpoint').removeAttribute('required');
-            document.getElementById('options').classList.remove("was-validated");
-        }
-    });
-
-    document.getElementById('sync_time').addEventListener('change', function () {
-        if (this.value < 1 || this.value !="") {
-            //validate the api endpoint cannot be empty
-            document.getElementById('options').classList.add("was-validated");
-        } else {
-            document.getElementById('options').classList.remove("was-validated");
-        }
+        document.getElementById("options").classList.add("was-validated");
+      } else {
+        document.getElementById("api_endpoint").value = "";
+        document.getElementById("api_endpoint").removeAttribute("required");
+        document.getElementById("options").classList.remove("was-validated");
+      }
     });
 
-    document.getElementById('monitor_time').addEventListener('change', function () {
-        if (this.value < 1 || this.value !="") {
-            //validate the api endpoint cannot be empty
-            document.getElementById('monitor_time').classList.add("was-validated");
-        } else {
-            document.getElementById('monitor_time').classList.remove("was-validated");
-        }
-    });
+  document.getElementById("sync_time").addEventListener("change", function () {
+    if (this.value < 1 ) {
+      document.getElementById("sync_time").value = 900;
+      document.getElementById("options").classList.add("was-validated");
+    } else {
+      document.getElementById("options").classList.remove("was-validated");
+    }
+  });
 
-    
-    
+  document
+    .getElementById("monitor_time")
+    .addEventListener("change", function () {
+      if (this.value < 1) {
+        document.getElementById("monitor_time").value = 60;
+        document.getElementById("monitor_time").classList.add("was-validated");
+      } else {
+        document
+          .getElementById("monitor_time")
+          .classList.remove("was-validated");
+      }
+    });
 }
-
 
 restore_options();
 //save settings
 var inputElements = document.getElementsByTagName("input");
 for (i = 0; i < inputElements.length; i++) {
-    inputElements[i].addEventListener("change", function(){
-        console.log("Save options triggered")
-        saveOptions();
-    })
+  inputElements[i].addEventListener("change", function () {
+    console.log("Save options triggered");
+    saveOptions();
+  });
 }
